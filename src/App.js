555e859e1f06db675135ee611bc692d53329599d
Faheem Hassan <faheemhassan1985@gmail.com>
2023-06-14 02:46:06 +0200

    used state to change component color onClick

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { QouteGenerator } from "./QouteContainer";
 
 import "./styles.css";
 import famousQoutations from "./Qoutes";
+import { useState } from "react";
+import { ColorPicker } from "./Qoutes";
 
 let x = Math.floor(Math.random() * 10 + 1);
+
 function App() {
+  const [color, setColor] = useState("red");
+  function newColor() {
+    setColor(ColorPicker());
+  }
   return (
     <div className="App">
       <QouteGenerator
@@ -14,6 +20,7 @@ function App() {
         qoute={famousQoutations[x].qoute}
         author={famousQoutations[x].author}
       />
+      <button onClick={newColor}>Next Qoute</button>
     </div>
   );
 }
